fa8fa0e67a93e715b6e67ec9964b226ba0dae9d1
Saurabh Patil <saurabhpatil97@hotmail.com>
2020-10-24 00:13:49 -0400

    Add graph

--- Flask_App/flask_server.py
+++ Flask_App/flask_server.py
@@ -4,6 +4,8 @@ import mysql.connector
 import sql_actions
 import datetime
 import uuid
+import matplotlib.pyplot as plt
+
 db = mysql.connector.connect(host='localhost', database='code_time', user='root', password='codetime')
 
 app = Flask(__name__)
@@ -30,28 +32,40 @@ def signup():
     unique_id = uuid.uuid4()
     unique_id = str(unique_id)
     sql_actions.add_data_users(db, unique_id)
-    return unique_id
+    return_id = {"your_id": unique_id}
+    return return_id
 
 
 @app.route('/dashboard/<user_id>', methods=['GET'])
 def dashboard(db, user_id):
     display_data = sql_actions.retrieve_data(db, user_id)
-    # matplotlib function
-    return display_data
+    # [(user_id, file_name, start_date, end_date)]
+    # Graph Plot Function
+    x_left = []
+    y_time = []
+    # labels for bars
+    tick_label = []
+    for i in range(len(display_data)):
+        x_left.append(i)
+        y_time.append(display_data[i][3])
+        tick_label.append(display_data[i][1])
+    plt.bar(x_left, y_time, tick_label=tick_label, width=0.8, color=['red', 'green'])
+    plt.xlabel('Files')
+    plt.ylabel('Time Worked')
+    plt.title(display_data[0][0])
+    plt.savefig('static/images/plot.png')
+    return render_template('plot.html', url='/static/images/plot.png')
 
 
 @app.route('/send', methods=['POST'])
 def send(db, user_id):
     send_list = request.get_json(force=True)
     print(send_list)
+    send_list = send_list.get_json()
     sql_actions.add_data_dashboard(db, send_list)
 
 
 if __name__ == "__main__":
-   # try:
-   #     db = mysql.connector.connect(host='localhost', database='code_time', user='root', password='codetime')
-   # except mysql.connector.Error as e:
-   #     print(e)
     server_ip = '0.0.0.0'
     server_port = 8080
     app.run(host=server_ip, port=server_port, debug=True)
