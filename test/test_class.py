1d8450d06bad4bad7e01983e18b6f63d87898c2f
Saurabh Patil <saurabhpatil97@hotmail.com>
2020-10-24 14:45:58 -0400

    Update test

--- test/test_class.py
+++ test/test_class.py
@@ -1,10 +1,9 @@
 import json
 from json import JSONEncoder
-from SublimePlugin import PeriodicLogSaver
-# write_log_file
+from SublimePlugin.periodicLogSaver import PeriodicLogSaver
 
 
 class TestClass:
 
     def test_one(self):
-        print("##########\ninitial test\n###########\n")
+        print("##########\ninitial test: PeriodicLogSaver.write_log_file\n###########\n")
