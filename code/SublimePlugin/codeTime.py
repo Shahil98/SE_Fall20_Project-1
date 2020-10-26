a9c9b05eb3b25cf083e8745f95353bda7554c709
Shahil <shahilushah@gmail.com>
2020-10-26 19:11:24 -0400

    Update primary functions

--- code/SublimePlugin/codeTime.py
+++ code/SublimePlugin/codeTime.py
@@ -4,16 +4,14 @@ from datetime import datetime as dt
 import sys
 from .periodicLogSaver import PeriodicLogSaver
 import json
-import mimetypes
 import urllib
 import platform
 import os
 
 # define local variables
 file_times_dict = {}
-periodic_log_save_timeout = 300  # seconds
+periodic_log_save_timeout = 10
 periodic_log_save_on = True
-
 user_id = ""
 
 if platform.system() == 'Windows':
@@ -29,9 +27,9 @@ try:
     user_id = f.readline()
     f.close()
 except:
-    print("Exception occured")
     f = open(DATA_FOLDER_PATH+"\\userid.txt", "w")
-    response = (urllib.request.urlopen("http://152.46.17.237:8080/signup").read())
+    response = (urllib.request.urlopen(
+        "http://152.46.17.237:8080/signup").read())
     user_id = (response.decode('utf-8'))
     f.write(user_id)
     f.close()
@@ -46,7 +44,6 @@ def when_activated(view):
     view: View class object
     """
     try:
-        print(file_times_dict.keys())
         window = view.window()
         if window is not None:
             file_name = view.file_name()
@@ -59,9 +56,6 @@ def when_activated(view):
                     file_times_dict[file_name].append([start_time, end_time])
                 else:
                     file_times_dict[file_name] = [[start_time, end_time]]
-
-                print('File_name: ', file_name)
-                print('\n ----- \n')
     except Exception as e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
         print("codeTime:when_activated(): {error} on line number: {lno}".format(error=str(e), lno=str(exc_tb.tb_lineno)))  # noqa: E501
@@ -83,9 +77,6 @@ def when_deactivated(view):
             if file_name is not None:
                 end_time = dt.now().strftime('%Y-%m-%d %H:%M:%S')
                 file_times_dict[file_name][-1][1] = end_time
-
-                print('File_name: ', file_name)
-                print('\n ----- \n')
     except Exception as e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
         print("codeTime:when_deactivated(): {error} on line number: {lno}".format(error=str(e), lno=str(exc_tb.tb_lineno)))  # noqa: E501
@@ -140,29 +131,22 @@ class CustomEventListener(sublime_plugin.EventListener):
 
             file_name = view.file_name()
             end_time = dt.now().strftime('%Y-%m-%d %H:%M:%S')
-            print(file_times_dict)
             if file_name is not None and file_name in file_times_dict:
                 if file_times_dict[file_name][-1][1] is None:
                     file_times_dict[file_name][-1][1] = end_time
-                json_insert_data = {}
-                json_insert_data["data"] = []
-                file_type=(file_name.split('\\')[-1]).split('.')[-1]
-                """
-                file_type = mimetypes.guess_type(file_name)
-                if file_type[0] is not None:
-                    file_type = file_type[0].split("/")[-1].split("-")[-1]
-                else:
-                    file_type = "other"
-                """
-                for i in range(len(file_times_dict[file_name])):
-                    json_insert_data["data"].append({'uid':user_id, "file_name": file_name,
-                                                     "start_date": file_times_dict[file_name][i][0], "end_date": file_times_dict[file_name][i][1],  "file_type": file_type })
+                json_insert_data = []
 
+                file_type = "."+(file_name.split('\\')[-1]).split('.')[-1]
+
+                for i in range(len(file_times_dict[file_name])):
+                    if(file_times_dict[file_name][i][1] == None):
+                        file_times_dict[file_name][i][1] = dt.now().strftime('%Y-%m-%d %H:%M:%S')
+                    json_insert_data.append({'uid': user_id, "file_name": file_name,
+                                             "start_date": file_times_dict[file_name][i][0], "end_date": file_times_dict[file_name][i][1],  "file_type": file_type})
                 data = json.dumps(json_insert_data).encode('utf-8')
-                req = urllib.request.Request("http://152.46.17.237:8080/send", data=data,
-                             headers={'content-type': 'application/json'})
+                req = urllib.request.Request(
+                    "http://152.46.17.237:8080/send", data=data, headers={'content-type': 'application/json'})
                 response = urllib.request.urlopen(req)
-                print(response)
                 file_times_dict.pop(file_name)
         except Exception as e:
             exc_type, exc_obj, exc_tb = sys.exc_info()
@@ -170,8 +154,6 @@ class CustomEventListener(sublime_plugin.EventListener):
 
 
 def plugin_loaded():
-
-    print("Plugin Loaded")
     try:
         if periodic_log_save_on:
             periodcLogSaver = PeriodicLogSaver(kwargs={'inMemoryLog': file_times_dict, 'timeout': periodic_log_save_timeout, 'user_id': user_id})  # noqa: E501
--- code/SublimePlugin/periodicLogSaver.py
+++ code/SublimePlugin/periodicLogSaver.py
@@ -6,12 +6,18 @@ from datetime import datetime as dt
 import sys
 import json
 import urllib
-import mimetypes
 
 
 class PeriodicLogSaver(threading.Thread):
 
     def __init__(self, group=None, target=None, name=None, args=(), kwargs=None, verbose=None):  # noqa: E128
+        """
+        Constructor for PeriodicLogSaver class.
+
+        Parameters
+        ----------
+        kwargs: Dictionary containing dictionary of files and times, time period for log saver and user id
+        """
         super(PeriodicLogSaver, self).__init__(
             group=group, target=target, name=name)
         self.args = args
@@ -20,7 +26,6 @@ class PeriodicLogSaver(threading.Thread):
 
     def run(self):
         while True:
-            print("In periodic log saver")
             try:
                 curr_file = sublime.active_window().active_view().file_name()
 
@@ -44,28 +49,28 @@ class PeriodicLogSaver(threading.Thread):
                 print("periodicLogSaver:PeriodicLogSaver:run(): {error} on line number: {lno}".format(error=str(e), lno=str(exc_tb.tb_lineno)))  # noqa: E501
 
     def write_log_file(self, file_times_dict):
-        try:
-            json_insert_data = {}
-            json_insert_data["data"] = []
-            for file_name in file_times_dict.keys():
-                file_type = file_type=(file_name.split('\\')[-1]).split('.')[-1]
-                """
-                if file_type[0] is not None:
-                    file_type = file_type[0].split("/")[-1].split("-")[-1]
-                else:
-                    file_type = "other"
-                """
-                for i in range(len(file_times_dict[file_name])):
-                    json_insert_data["data"].append({'uid':user_id, "file_name": file_name,
-                                                     "start_date": file_times_dict[file_name][i][0], "end_date": file_times_dict[file_name][i][1],  "file_type": file_type })
+        """
+        Function to send data to server
 
+        Parameters
+        ----------
+        file_times_dict: dictionary with files and corresponding time information
+        """
+        try:
+            json_insert_data = []
+            for file_name in file_times_dict.keys():
+                file_type = "."+(file_name.split('\\')[-1]).split('.')[-1]
+                for i in range(len(file_times_dict[file_name])):
+                    if(file_times_dict[file_name][i][1] == None):
+                        file_times_dict[file_name][i][1] = dt.now().strftime('%Y-%m-%d %H:%M:%S')
+                    json_insert_data.append({'uid': self.kwargs["user_id"], "file_name": file_name,
+                                             "start_date": file_times_dict[file_name][i][0], "end_date": file_times_dict[file_name][i][1],  "file_type": file_type})
             data = json.dumps(json_insert_data).encode('utf-8')
-            req = urllib.request.Request("http://152.46.17.237:8080/send", data=data, headers={'content-type': 'application/json'})
+            req = urllib.request.Request(
+                "http://152.46.17.237:8080/send", data=data, headers={'content-type': 'application/json'})
             response = urllib.request.urlopen(req)
             return response.getcode()
-            # json_insert_data = json.dumps(json_insert_data)
-            # print(json_insert_data)
-            # requests.post("http://152.46.17.237:8080/send", json=json_insert_data)
+
         except Exception as e:
             exc_type, exc_obj, exc_tb = sys.exc_info()
             print("periodicLogSaver:PeriodicLogSaver():write_log_file(): {error} on line number: {lno}".format(error=str(e), lno=str(exc_tb.tb_lineno)))  # noqa: E501
