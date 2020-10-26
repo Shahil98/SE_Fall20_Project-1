85ea49d26b55d7eedc63a6ae07d1272767b14d7a
Shahil <shahilushah@gmail.com>
2020-10-26 00:02:15 -0400

    Add code files

--- code/SublimePlugin/codeTime.py
+++ code/SublimePlugin/codeTime.py
@@ -1,201 +1,151 @@
 import sublime_plugin
 import sublime
-import time
-import platform
-import os
 from datetime import datetime as dt
 import sys
-import subprocess
-
+import requests
 from .periodicLogSaver import PeriodicLogSaver
-
-# create data folder based on OS
-if platform.system() == "Windows":
-    DATA_FOLDER_PATH = os.path.join(os.getenv("APPDATA"), ".codeTime")
-else:
-    DATA_FOLDER_PATH = os.path.join(os.path.expanduser("~"), ".codeTime")
-
-if not os.path.exists(DATA_FOLDER_PATH):
-    os.makedirs(DATA_FOLDER_PATH)
-
-# define log file path
-LOG_FILE_PATH = os.path.join(DATA_FOLDER_PATH, ".sublime_logs")
+import json
+import mimetypes
 
 # define local variables
 file_times_dict = {}
 periodic_log_save_timeout = 300  # seconds
 periodic_log_save_on = True
-api_token = ""
-configs = None
 
 
 def when_activated(view):
+    """
+    Function to log start timestamp when a window is activated.
+
+    Parameters
+    ----------
+    view: View class object
+    """
     try:
+        print(file_times_dict.keys())
         window = view.window()
         if window is not None:
             file_name = view.file_name()
 
             if file_name is not None:
-                start_time = time.time()
                 end_time = None
+                start_time = dt.now().strftime('%Y-%m-%d %H:%M:%S')
 
-                curr_date = dt.now().strftime("%Y-%m-%d")
-
-                if curr_date not in file_times_dict:
-                    file_times_dict[curr_date] = {}
-
-                if file_name not in file_times_dict[curr_date]:
-
-                    file_times_dict[curr_date][file_name] = [
-                        [start_time, end_time]
-                    ]  # noqa: E501
+                if(file_name in file_times_dict):
+                    file_times_dict[file_name].append([start_time, end_time])
                 else:
-                    file_times_dict[curr_date][file_name].append(
-                        [start_time, end_time]
-                    )  # noqa: E501
+                    file_times_dict[file_name] = [[start_time, end_time]]
 
-                print("File_name: ", file_name)
-                print("\n ----- \n")
+                print('File_name: ', file_name)
+                print('\n ----- \n')
     except Exception as e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
-        print(
-            "codeTime:when_activated(): {error} on line number: {lno}".format(
-                error=str(e), lno=str(exc_tb.tb_lineno)
-            )
-        )  # noqa: E501
+        print("codeTime:when_activated(): {error} on line number: {lno}".format(error=str(e), lno=str(exc_tb.tb_lineno)))  # noqa: E501
 
 
 def when_deactivated(view):
+    """
+    Function to log end timestamp when a window is deactivated.
+
+    Parameters
+    ----------
+    view: View class object
+    """
     try:
         window = view.window()
         if window is not None:
             file_name = view.file_name()
 
             if file_name is not None:
-                end_time = time.time()
+                end_time = dt.now().strftime('%Y-%m-%d %H:%M:%S')
+                file_times_dict[file_name][-1][1] = end_time
 
-                curr_date = dt.now().strftime("%Y-%m-%d")
-
-                file_times_dict[curr_date][file_name][-1][1] = end_time
-
-                print("File_name: ", file_name)
-                print("\n ----- \n")
+                print('File_name: ', file_name)
+                print('\n ----- \n')
     except Exception as e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
-        print(
-            "codeTime:when_deactivated(): {error} on line number: {lno}".format(
-                error=str(e), lno=str(exc_tb.tb_lineno)
-            )
-        )  # noqa: E501
+        print("codeTime:when_deactivated(): {error} on line number: {lno}".format(error=str(e), lno=str(exc_tb.tb_lineno)))  # noqa: E501
 
 
 class CustomEventListener(sublime_plugin.EventListener):
-    def on_post_save(self, view):
-        print(view.file_name(), "just got saved")
+    """
+    Class which is a subclass of EventListener that has functions for save, window
+    activate, window deactivate and window close events.
+    """
 
     def on_activated(self, view):
+        """
+        Function which uses when_activated function to log data.
+
+        Parameters
+        ----------
+        view: View class object
+        """
         try:
-            print(view.file_name(), "is now the active view")
+            print(view.file_name(), 'is now the active view')
             when_activated(view)
         except Exception as e:
             exc_type, exc_obj, exc_tb = sys.exc_info()
-            print(
-                "codeTime:CustomEventListener():on_activated() {error} on line number: {lno}".format(
-                    error=str(e), lno=str(exc_tb.tb_lineno)
-                )
-            )  # noqa: E501
+            print("codeTime:CustomEventListener():on_activated() {error} on line number: {lno}".format(error=str(e), lno=str(exc_tb.tb_lineno)))  # noqa: E501
 
     def on_deactivated(self, view):
+        """
+        Function which uses when_deactivated function to log data.
+
+        Parameters
+        ----------
+        view: View class object
+        """
         try:
-            print(view.file_name(), "is deactivated view")
+            print(view.file_name(), 'is deactivated view')
             when_deactivated(view)
         except Exception as e:
             exc_type, exc_obj, exc_tb = sys.exc_info()
-            print(
-                "codeTime:CustomEventListener():on_deactivated() {error} on line number: {lno}".format(
-                    error=str(e), lno=str(exc_tb.tb_lineno)
-                )
-            )  # noqa: E501
+            print("codeTime:CustomEventListener():on_deactivated() {error} on line number: {lno}".format(error=str(e), lno=str(exc_tb.tb_lineno)))  # noqa: E501
 
     def on_close(self, view):
+        """
+        Function which sends the data to the databse server when a window is closed.
+
+        Parameters
+        ----------
+        view: View class object
+        """
         try:
-            print(view.file_name(), "is no more")
+            print(view.file_name(), 'is no more')
 
             file_name = view.file_name()
-            curr_date = dt.now().strftime("%Y-%m-%d")
-            if (
-                    file_name is not None
-                    and file_name in file_times_dict[curr_date]
-            ):
-
-                end_time = time.time()
-
-                last_time_list = file_times_dict[curr_date][file_name][-1]
-
-                if last_time_list[1] is None:
-                    last_time_list[1] = end_time
-
-                with open(LOG_FILE_PATH, "a") as f:
-                    for _time in file_times_dict[curr_date][file_name]:
-                        f.write(
-                            curr_date
-                            + ","
-                            + file_name
-                            + ","
-                            + str(_time[0])
-                            + ","
-                            + str(_time[1])
-                            + "\n"
-                        )  # noqa: E501
-                file_times_dict[curr_date].pop(file_name, None)
+            end_time = dt.now().strftime('%Y-%m-%d %H:%M:%S')
+            if file_name is not None and file_name in file_times_dict:
+                if file_times_dict[file_name][-1][1] is None:
+                    file_times_dict[file_name][-1][1] = end_time
+                json_insert_data = {}
+                json_insert_data["data"] = []
+                file_type = mimetypes.guess_type(file_name)
+                if file_type[0] is not None:
+                    file_type = file_type[0].split("/")[-1].split("-")[-1]
+                else:
+                    file_type = "other"
+                for i in range(len(file_times_dict[file_name])):
+                    json_insert_data["data"].append({"file_name": file_name, "file_type": file_type,
+                                                     "start_time": file_times_dict[file_name][i][0], "end_time": file_times_dict[file_name][i][1]})
+                json_insert_data = json.dumps(json_insert_data)
+                print(json_insert_data)
+                requests.post("http://152.46.17.237:8080/insert_data",
+                              json=json_insert_data)
+                file_times_dict.pop(file_name)
         except Exception as e:
             exc_type, exc_obj, exc_tb = sys.exc_info()
-            print(
-                "codeTime:CustomEventListener():on_close() {error} on line number: {lno}".format(
-                    error=str(e), lno=str(exc_tb.tb_lineno)
-                )
-            )  # noqa: E501
-
-
-# view.run_command('dashboard')
-class DashboardCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        # Load config file
-        configs = sublime.load_settings("CustomPreferences.sublime-settings")
-        try:
-            print("Showing Graphs")
-            dir_path = os.path.dirname(os.path.realpath(__file__))
-            subprocess.Popen(
-                configs.get("python-env") + "'" + dir_path + "/output.py'",
-                shell=True,
-                stdout=subprocess.PIPE,
-            )  # noqa: E501, F841
-        except Exception as e:
-            exc_type, exc_obj, exc_tb = sys.exc_info()
-            print("codeTime:DashboardCommand():run() {error} on line number: {lno}".format(error=str(e), lno=str(exc_tb.tb_lineno)))
+            print("codeTime:CustomEventListener():on_close() {error} on line number: {lno}".format(error=str(e), lno=str(exc_tb.tb_lineno)))  # noqa: E501
 
 
 def plugin_loaded():
+
+    print("Plugin Loaded")
     try:
-        configs = sublime.load_settings("CustomPreferences.sublime-settings")
-        periodic_log_save_timeout = configs.get("timeout")
-        api_token = configs.get("api_token")
-        request_url = configs.get("request-url")
         if periodic_log_save_on:
-            periodcLogSaver = PeriodicLogSaver(
-                kwargs={
-                    "inMemoryLog": file_times_dict,
-                    "timeout": periodic_log_save_timeout,
-                    "LOG_FILE_PATH": LOG_FILE_PATH,
-                    "API_TOKEN": api_token,
-                    "REQUEST_URL": request_url,
-                }
-            )  # noqa: E501
+            periodcLogSaver = PeriodicLogSaver(kwargs={'inMemoryLog': file_times_dict, 'timeout': periodic_log_save_timeout})  # noqa: E501
             periodcLogSaver.start()
     except Exception as e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
-        print(
-            "codeTime:plugin_loaded() {error} on line number: {lno}".format(
-                error=str(e), lno=str(exc_tb.tb_lineno)
-            )
-        )  # noqa: E501
+        print("codeTime:plugin_loaded() {error} on line number: {lno}".format(error=str(e), lno=str(exc_tb.tb_lineno)))  # noqa: E501
--- code/SublimePlugin/periodicLogSaver.py
+++ code/SublimePlugin/periodicLogSaver.py
@@ -2,121 +2,65 @@ import threading
 import time
 import copy
 import sublime
-import json
-import urllib.request
 from datetime import datetime as dt
 import sys
+import json
+import requests
+import mimetypes
 
 
 class PeriodicLogSaver(threading.Thread):
-    def __init__(
-            self,
-            group=None,
-            target=None,
-            name=None,
-            args=(),
-            kwargs=None,
-            verbose=None,
-    ):
+
+    def __init__(self, group=None, target=None, name=None, args=(), kwargs=None, verbose=None):  # noqa: E128
         super(PeriodicLogSaver, self).__init__(
-            group=group, target=target, name=name
-        )
+            group=group, target=target, name=name)
         self.args = args
         self.kwargs = kwargs
         return
 
     def run(self):
         while True:
+            print("In periodic log saver")
             try:
                 curr_file = sublime.active_window().active_view().file_name()
-                curr_date = dt.now().strftime("%Y-%m-%d")
 
                 if curr_file is not None:
-                    inMemLog = self.kwargs["inMemoryLog"]
-                    inMemoryLogDeepCopy = copy.deepcopy(inMemLog)
-                    inMemoryLog = inMemLog
+                    inMemoryLogDeepCopy = copy.deepcopy(
+                        self.kwargs['inMemoryLog'])
+                    inMemoryLog = self.kwargs['inMemoryLog']
                     inMemoryLog.clear()
 
-                    # Writing the current date and file inMemoryLogDeepCopy by calling write_log_file
+                    if curr_file in inMemoryLogDeepCopy:  # noqa: E501
+                        end_time = dt.now().strftime('%Y-%m-%d %H:%M:%S')
+                        inMemoryLogDeepCopy[curr_file][-1][1] = end_time
 
-                    if (
-                            curr_date in inMemoryLogDeepCopy
-                            and curr_file in inMemoryLogDeepCopy[curr_date]
-                    ):
-                        end_time = time.time()
-                        cd = curr_date
-                        cf = curr_file
-                        inMemoryLogDeepCopy[cd][cf][-1][1] = end_time
-
-                        if curr_date not in inMemoryLog:
-                            inMemoryLog[curr_date] = {}
-
-                        if curr_file not in inMemoryLog[curr_date]:
-                            temp = [[end_time, None]]
-                            inMemoryLog[curr_date][curr_file] = temp
+                        if curr_file not in inMemoryLog:
+                            inMemoryLog[curr_file] = [[end_time, None]]
 
                         self.write_log_file(inMemoryLogDeepCopy)
-                time.sleep(self.kwargs["timeout"])
+                time.sleep(self.kwargs['timeout'])
             except Exception as e:
                 exc_type, exc_obj, exc_tb = sys.exc_info()
-                print(
-                    "periodicLogSaver:PeriodicLogSaver:run(): {error} on line \
-                        number: {lno}".format(
-                        error=str(e), lno=str(exc_tb.tb_lineno)
-                    )
-                )
+                print("periodicLogSaver:PeriodicLogSaver:run(): {error} on line number: {lno}".format(error=str(e), lno=str(exc_tb.tb_lineno)))  # noqa: E501
 
     def write_log_file(self, file_times_dict):
         try:
-
-            obj = []
-            api_token = self.kwargs["API_TOKEN"]
-            file_type = (
-                sublime.active_window().active_view().settings().get("syntax")
-            )
-            with open(self.kwargs["LOG_FILE_PATH"], "a") as f:
-
-                for key, val in file_times_dict.items():
-                    curr_date = key
-                    file_dict = val
-
-                    for file_name, times_list in file_dict.items():
-                        for time_start_end in times_list:
-                            f.write(
-                                curr_date
-                                + ","
-                                + file_name
-                                + ","
-                                + str(time_start_end[0])
-                                + ","
-                                + str(time_start_end[1])
-                                + "\n"
-                            )  # noqa: E501
-                            row = {}
-                            row["file_name"] = file_name.split("/")[-1]
-                            row["file_extension"] = file_name.split(".")[-1]
-                            row["detected_language"] = file_type.split("/")[
-                                -1
-                            ].split(".")[-2]
-                            row["log_date"] = curr_date
-                            row["log_timestamp"] = str(time_start_end[0])
-                            row["api_token"] = api_token
-                            obj.append(row)
-
-                req = urllib.request.Request(self.kwargs["REQUEST_URL"])
-                req.add_header(
-                    "Content-Type", "application/json; charset=utf-8"
-                )
-                jsondata = json.dumps(obj)
-                jsondataasbytes = jsondata.encode("utf-8")
-                req.add_header("Content-Length", len(jsondataasbytes))
-                response = (urllib.request.urlopen(req, jsondataasbytes).read().decode()) # noqa F841
+            json_insert_data = {}
+            json_insert_data["data"] = []
+            for file_name in file_times_dict.keys():
+                file_type = mimetypes.guess_type(file_name)
+                if file_type[0] is not None:
+                    file_type = file_type[0].split("/")[-1].split("-")[-1]
+                else:
+                    file_type = "other"
+                for i in range(len(file_times_dict[file_name])):
+                    json_insert_data["data"].append({"file_name": file_name, "file_type": file_type,
+                                                     "start_time": file_times_dict[file_name][i][0], "end_time": file_times_dict[file_name][i][1]})
+                    json_insert_data = json.dumps(json_insert_data)
+                    print(json_insert_data)
+                    requests.post(
+                        "http://152.46.17.237:8080/insert_data", json=json_insert_data)
 
         except Exception as e:
             exc_type, exc_obj, exc_tb = sys.exc_info()
-            print(
-                "periodicLogSaver:PeriodicLogSaver():write_log_file(): {error} \
-                on line number: {lno}".format(
-                    error=str(e), lno=str(exc_tb.tb_lineno)
-                )
-            )  # noqa: E501
+            print("periodicLogSaver:PeriodicLogSaver():write_log_file(): {error} on line number: {lno}".format(error=str(e), lno=str(exc_tb.tb_lineno)))  # noqa: E501
