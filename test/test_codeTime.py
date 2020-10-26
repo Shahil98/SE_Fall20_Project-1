0104c87f89b7ae1c8f7d3011653e2d8b830b704f
Shahil Shah <shahilushah@gmail.com>
2020-10-25 20:55:37 -0400

    Update test_codeTime.py

--- test/test_codeTime.py
+++ test/test_codeTime.py
@@ -4,16 +4,15 @@ import io
 from datetime import datetime as dt
 from unittest import TestCase
 from unittest.mock import Mock, patch
-from code.SublimePlugin import codeTime
 
 version = sublime.version()
-# modules = sys.modules.keys()
-# capturedOutput = io.StringIO()
-# sys.stdout = capturedOutput
-# print("\n$$$$$$\n", modules, "\n$$$$$$$$\n")
-# sys.stdout = sys.__stdout__
-# print('Captured', capturedOutput.getvalue())
-# codeTime = sys.modules["SE_Fall20_Project-1.code.SublimePlugin.codeTime"]
+ modules = sys.modules.keys()
+ capturedOutput = io.StringIO()
+ sys.stdout = capturedOutput
+ print("\n$$$$$$\n", modules, "\n$$$$$$$$\n")
+ sys.stdout = sys.__stdout__
+ print('Captured', capturedOutput.getvalue())
+ codeTime = sys.modules["SE_Fall20_Project-1.code.SublimePlugin.codeTime"]
 
 
 class TestFunctions(TestCase):
