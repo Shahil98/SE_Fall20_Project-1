55833fb6e61866e66e128d279dc06814223d21db
Saurabh-97 <saurabhpatil97@hotmail.com>
2020-10-28 02:02:53 -0400

    Update periodic.py

--- test/periodic.py
+++ test/periodic.py
@@ -3,11 +3,8 @@ import json
 
 
 def tester(data):
-    # uid1 = 'cdfd2609-e7aa-4dff-af50-649a89bd8805'
-    # body = [{"uid": uid1, "file_name": "saurabh.py", "start_date": "2020-09-26 16:15:00", "end_date": "2020-09-26 19:00:00", "file_type": ".py"}]
     body1 = json.dumps(data).encode('utf-8')
     code = 200
-    # r = requests.post('http://152.46.17.237:8080/send', json=body1)
     req = urllib.request.Request("http://152.46.17.237:8080/send", data=body1, headers={'content-type': 'application/json'})
     response = urllib.request.urlopen(req)
     code1 = response.getcode()
