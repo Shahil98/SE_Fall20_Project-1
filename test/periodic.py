53dc603c42db5c40e49843a268c0e1f79835d310
Saurabh Patil <saurabhpatil97@hotmail.com>
2020-10-28 00:55:31 -0400

    Update test6.2

--- test/periodic.py
+++ test/periodic.py
@@ -13,13 +13,3 @@ def tester(data):
     code1 = response.getcode()
     print(code1)
     return code
-
-
-def tester_tester():
-    body = [{"file_name": "saurabh.py", "file_type": ".py"}]
-    body1 = json.dumps(body).encode('utf-8')
-    req = urllib.request.Request("http://152.46.17.237:8080/send", data=body1, headers={'content-type': 'application/json'})
-    response = urllib.request.urlopen(req)
-    code1 = response.getcode()
-    print(code1)
-    return 200
--- test/periodic_test.py
+++ test/periodic_test.py
@@ -4,5 +4,5 @@ uid1 = 'cdfd2609-e7aa-4dff-af50-649a89bd8805'
 body = [{"uid": uid1, "file_name": "saurabh.py", "start_date": "2020-09-26 17:15:00", "end_date": "2020-09-26 19:00:00", "file_type": ".py"}]
 code = pt.tester(body)
 assert code == 200
-code = pt.tester_tester()
-assert code == 200
+# code = pt.tester_tester()
+# assert code == 200
